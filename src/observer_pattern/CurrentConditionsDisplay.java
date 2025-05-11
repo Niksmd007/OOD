a54a8a95c79e9a0b17cdd7c8b7c50f4d56c2a883
Nikhil Kumar <nikhil.kumar3@walmart.com>
2025-05-11 17:44:37 +0530

    observer pattern

---
+++ src/observer_pattern/CurrentConditionsDisplay.java
@@ -0,0 +1,23 @@
+package observer_pattern;
+
+public class CurrentConditionsDisplay implements Observer,DisplayElement {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+      this.temperature=temperature;
+      this.humidity=humidity;
+      display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: "+temperature+"F degree and "+humidity+"% humidity");
+    }
+}
